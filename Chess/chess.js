1f4b33c6f3e4e65f29e9d2d9ea6a219224185cba
WajdiAmer <wajdi.amerrr@gmail.com>
2022-04-20 04:56:03 +0300

    Added possible movements highlighting to PAWN and ROOK

--- Chess/chess.js
+++ Chess/chess.js
@@ -20,7 +20,72 @@ class Piece {
     this.type = type;
     this.team = team;
   }
+
+  getPossibleMoves() {
+    let relativeMoves;
+    if (this.type === PAWN) {
+      relativeMoves = this.getPawnRelativeMoves();
+    } else if (this.type === ROOK) {
+      relativeMoves = this.getRookRelativeMoves();
+    } else if (this.type === KNIGHT) {
+      // TODO: Get moves
+    } else if (this.type === BISHOP) {
+      // TODO: Get moves
+    } else if (this.type === KING) {
+      // TODO: Get moves
+    } else if (this.type === QUEEN) {
+      // TODO: Get moves
+    } else {
+      console.log("Unknown type", type)
+    }
+
+    let absoluteMoves = [];
+    for (let relativeMove of relativeMoves) {
+      const absoluteRow = this.row + relativeMove[0];
+      const absoluteCol = this.col + relativeMove[1];
+      absoluteMoves.push([absoluteRow, absoluteCol]);
+    }
+
+    let filteredMoves = [];
+    for (let absoluteMove of absoluteMoves) {
+      const absoluteRow = absoluteMove[0];
+      const absoluteCol = absoluteMove[1];
+      if (absoluteRow >= 0 && absoluteRow <= 7 && absoluteCol >= 0 && absoluteCol <= 7) {
+        filteredMoves.push(absoluteMove);
+      }
+    }
+    return filteredMoves;
+  }
+
+  getPawnRelativeMoves() {
+      // TODO: Give different answer to black player
+      return [[1, 0]];
 }
+
+  getRookRelativeMoves() {
+    let result = [];
+    for (let i = 1; i < BOARD_SIZE; i++) {
+      result.push([i, 0]);
+      result.push([-i, 0]);
+      result.push([0, i]);
+      result.push([0, -i]);
+  }
+  return result;
+  }
+}
+
+                //BOARD DATA TASK..
+// class BoardData {
+//   constructor(pieces) {
+//     this.pieces = pieces;
+//   }
+
+//   // Returns piece in row, col, or undefined if not exists.
+//   getPiece(row, col) {
+
+//   }
+// }
+
 
 function getInitialBoard() {
   let result = [];
@@ -53,22 +118,22 @@ function addImage(cell, team, type) {
   cell.appendChild(image);
 }
 
-function addImageByIndex(cell, team, index) {
-  if (index === 0 || index === 7) {
-    addImage(cell, team, ROOK);
-  } else if (index === 1 || index === 6) {
-    addImage(cell, team, KNIGHT);
-  } else if (index === 2 || index === 5) {
-    addImage(cell, team, BISHOP);
-  } else if (index === 3) {
-    addImage(cell, team, QUEEN);
-  } else if (index === 4) {
-    addImage(cell, team, KING);
+
+function onCellClick(e, row, col) {
+  for (let i = 0; i < BOARD_SIZE; i++) {
+    for (let j = 0; j < BOARD_SIZE; j++) {
+      table.rows[i].cells[j].classList.remove('possibleMovement');
+    }
   }
-}
+  
+  for (let piece of pieces) {
+    if (piece.row === row && piece.col === col) {
+      let possibleMoves = piece.getPossibleMoves();
+      for (let possibleMove of possibleMoves)
+      table.rows[possibleMove[0]].cells[possibleMove[1]].classList.add('possibleMovement');
+    }
+  }
 
-
-function onCellClick(e) {
   if (selectedCell !== undefined) {
     selectedCell.classList.remove('selectedCell');
   }
@@ -83,17 +148,17 @@ function createChessBoard() {
   document.body.appendChild(containerDiv);
   table = document.createElement('table');
   containerDiv.appendChild(table);
-  for (let i = 0; i < BOARD_SIZE; i++) {
-    const row = table.insertRow();
-    for (let j = 0; j < BOARD_SIZE; j++) {
-      const cell = row.insertCell();
-      cell.id = "cell-" + i.toString() + "_" + j.toString();
-      if ((i + j) % 2 === 0) {
+  for (let row = 0; row < BOARD_SIZE; row++) {
+    const rowElement = table.insertRow();
+    for (let col = 0; col < BOARD_SIZE; col++) {
+      const cell = rowElement.insertCell();
+      cell.id = "cell-" + row.toString() + "_" + col.toString();
+      if ((row + col) % 2 === 0) {
         cell.className = 'lightCell';
       } else {
         cell.className = 'darkCell';
       }
-        cell.addEventListener('click', onCellClick);
+        cell.addEventListener('click', (e) => onCellClick(e, row, col));
     }
   }
   pieces = getInitialBoard();
